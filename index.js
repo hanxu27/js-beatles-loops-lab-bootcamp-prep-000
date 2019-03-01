34b2be692d1f34c77b61fb56046960121c010079
han xu <hanxu27@gmail.com>
2019-03-01 02:53:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  
+  for (let i = 0, musicians.length, i++) {
+    array[i] = `musicians[i] plays instruments[i]`
+  }
 }
