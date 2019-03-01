507c71e0a21d6f51b36e8315b7a8b669706c27d6
han xu <hanxu27@gmail.com>
2019-03-01 03:09:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function johnLennonFacts(facts) {
   }
   return array
 }
+
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+  } while (--number < 15)
+  return array
+}
